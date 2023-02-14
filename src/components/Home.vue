b962a46df81e558dfa718a171fafd5118b87b0e9
syan-hang <63327637+syan-hang@users.noreply.github.com>
2023-02-14 16:17:41 +0800

    elementUi 按需使用 配置

---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>hello syan!</h1>
+    <el-button>hello</el-button>
+    <el-button type="primary">syan!</el-button>
+    <el-button type="info">syan!</el-button>
+    <el-button type="danger">syan!</el-button>
+    <el-button type="success">syan!</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '../plugins/element.js'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+/**
+ * elementUI 全局引入
+ * import ElementUI from 'element-ui'
+  import 'element-ui/lib/theme-chalk/index.css'
+  Vue.use(ElementUI) 
+ */
