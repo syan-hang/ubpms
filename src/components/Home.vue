d90265491eadf280463c091bd269daa97cb3a2dc
syan-hang <63327637+syan-hang@users.noreply.github.com>
2023-02-15 11:29:23 +0800

    框架搭建
    node版本从16降低至14，安装sass-loader@7以及node-sass@4

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -18,19 +18,11 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="scss">
+.hello{
+  background: yellow;
+  .button{
+    background: red;
+  }
 }
 </style>
