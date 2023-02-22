b8dbdfb388f17eb072e65478f27de838465a1ba2
syan-hang <63327637+syan-hang@users.noreply.github.com>
2023-02-22 16:38:42 +0800

    学生列表前端分页

--- src/components/students/StudentList.vue
+++ src/components/students/StudentList.vue
@@ -1,6 +1,6 @@
 <template>
-	<div>
-		<el-table :data="tableData" border style="width: 100%">
+	<div class="studentList">
+		<el-table :data="compTable" border style="width: 100%">
 			<el-table-column prop="name" label="姓名" align="center"> </el-table-column>
 			<el-table-column prop="age" label="年龄" align="center"> </el-table-column>
 			<el-table-column prop="sex_text" label="性别" align="center"> </el-table-column>
@@ -15,6 +15,15 @@
 				</template>
 			</el-table-column>
 		</el-table>
+		<el-pagination
+			@size-change="handleSizeChange"
+			@current-change="handleCurrentChange"
+			:current-page="currentPage"
+			:page-sizes="[5, 10, 20, 30, 50]"
+			:page-size="pageSize"
+			layout="total, sizes, prev, pager, next, jumper"
+			:total="total">
+		</el-pagination>
 	</div>
 </template>
 
@@ -24,17 +33,36 @@ export default {
 	data() {
 		return {
 			tableData: [],
+			currentPage: 1, // 当前页数
+			pageSize: 10, // 每页条数
+			total: 0, // 总数据条数
 		};
+	},
+	computed: {
+		compTable() {
+			return this.tableData.slice(
+				(this.currentPage - 1) * this.pageSize,
+				this.currentPage * this.pageSize
+			);
+		},
 	},
 	created() {
 		this.getData();
 	},
 	methods: {
+		handleSizeChange(val) {
+			this.pageSize = val;
+			this.currentPage = 1;
+		},
+		handleCurrentChange(val) {
+			this.currentPage = val;
+		},
 		getData(params) {
 			getStudentList(params).then((res) => {
 				console.log(res);
 				if (res.status === 200) {
 					this.tableData = res.data.data;
+					this.total = res.data.total;
 					this.tableData.forEach((item) => {
 						// 加字段而不是修改原数据
 						item.sex_text = item.sex === 1 ? "男" : "女";
@@ -47,3 +75,11 @@ export default {
 	},
 };
 </script>
+
+<style lang="scss">
+.studentList {
+	.el-pagination {
+		margin-top: 20px;
+	}
+}
+</style>
